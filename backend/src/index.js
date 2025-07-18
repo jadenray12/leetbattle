3453d9ed6342f3b58b1aeb20b13289706fcf4ee0
Jaden Ray <144746006+jadenray12@users.noreply.github.com>
2025-07-17 18:55:01 -0700

    added connection to db

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,18 @@
-require('dotenv').config()
+require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
+
 
 const app = require('./app');
-
+const mongoose = require('mongoose');
 
 
 const PORT = process.env.PORT || 3001;
-if (process.env.NODE_ENV !== 'test') {
+mongoose.connect("mongodb+srv://jaden:Sportsman4021@cluster0.eh0ddy1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then( () => {
+    if (process.env.NODE_ENV !== 'test') {
     app.listen(PORT, () => {
         console.log(`Listening on port ${PORT}`)
     })
-}
+    }
+
+})
+
